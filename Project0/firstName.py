3f176075faa06f398cb6b74274693806aa550042
Nic Garcia <nicroygarcia@gmail.com>
2021-10-11 11:29:00 -0700

    Lists and Tuples Exercises

---
+++ Project0/firstName.py
@@ -0,0 +1,5 @@
+My_Name = "Nic"
+print(My_Name.title())
+print(My_Name.upper())
+print(My_Name.lower())
+
---
+++ Project0/oneTwo.py
@@ -0,0 +1,4 @@
+My_String = "hello nic is the coolest person ever"
+print(My_String)
+My_String = "hello jhila is very very very very cool"
+print(My_String)
