872ea1d777c1fd2c412f3dbe5d30f7a6dc92567e
helloooonicccccc <nicroygarcia@gmail.com>
2022-05-04 13:08:29 -0700

    Write Take Manual Turn method to take in a column and find calculate the right row.

--- ConnectFour/connectfour.py
+++ ConnectFour/connectfour.py
@@ -3,7 +3,6 @@ import random
 
 class ConnectFour:
     def __init__(self):
-        # TODO: Set up the board to be '-'
         self.board = []
         self.columns = 8
         self.rows = 8
@@ -16,13 +15,11 @@ class ConnectFour:
             self.board.append(row)
 
     def print_instructions(self):
-        # TODO: Print the instructions to the game
         print("Rules of Connect Four: You can play another player or an AI on an 8X8 board")
         print("2)  to win, you must create a diagonal, horizontal, or vertical line of 4")
         print("3) the game ends when either player wins")
 
     def print_board(self):
-        # TODO: Print the board
         print("   0 1 2 3 4 5 6 7")
         for t in range(self.columns):
             print(str(t), end='  ')
@@ -30,17 +27,55 @@ class ConnectFour:
 
 
     def is_valid_move(self, row, col):
+        if (self.board[row][col] == '-'):
+            return True
+        return False
 
     def place_player(self, player, row, col):
 
+        if (player == 0):
+            self.board[row][col] = 'B'
+
+        if (player == 1):
+            self.board[row][col] = 'R'
+
+        if (player == '-'):
+            self.board[row][col] = '-'
 
     def take_manual_turn(self, player):
 
+        while (True):
+            try:
+                full = True
+                while(full == True):
+                    colInput = int(input("What column to drop: "))
+                    if (self.board[0][colInput] != '-'):
+                        print("That row is full go again")
+                    else:
+                        full = False
+                for i in range(7,-1,-1):
+                    if (self.board[i][colInput] == '-'):
+                        rowInput = i
+                if (player == 0):
+                    self.place_player(0, rowInput, colInput)
+                elif (player == 1):
+                    self.place_player(1, rowInput, colInput)
+
+            except IndexError:
+                print("this is not a valid response")
+                continue
+            except ValueError:
+                print("this is not a valid response")
+                continue
+            return
+
 
     def take_turn(self, player):
+        print()
+        print("Player 1, take your turn:")
+        self.take_manual_turn(player)
 
     def check_col_win(self, player):
-        # TODO: Check col win
 
     def check_row_win(self, player):
 
